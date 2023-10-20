da1c5669b0a0860d73b26637ad34ad00a84e5051
danGit <dantonosu@gmail.com>
2023-10-20 11:47:59 -0700

    add database helper

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace EvernoteClone.Model
 {
-    internal class User
+    public class User
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
     }
 }
---
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,78 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.ViewModel.Helpers
+{
+    public class DatabaseHelper
+    {
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            using(SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Insert(item);
+                if(rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Update(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                int rows = connection.Delete(item);
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+            //where T : new() ensures that T is parameterless constructor, a requirement to call Table<T>()
+        public static List<T>Read<T>() where T : new()
+        {
+
+            List<T> items;
+            using (SQLiteConnection connection = new SQLiteConnection(dbFile))
+            {
+                connection.CreateTable<T>();
+                items = connection.Table<T>().ToList();
+            }
+
+            return items;
+        }
+
+    }
+}
