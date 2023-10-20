bab91813d91b51c7f609b09e1cd9b946abe50155
danGit <dantonosu@gmail.com>
2023-10-20 11:17:28 -0700

    models

---
+++ EvernoteClone/Model/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int NoteBookId { get; set; }
+
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
